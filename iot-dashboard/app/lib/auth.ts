b4d26519489472d95ea967746dd86c796b29a9c4
TrungTan369 <ttqpentakill@gmail.com>
2025-11-21 17:02:22 +0700

    rm ioi dashboard

--- iot-dashboard/app/lib/auth.ts
+++
@@ -1,8 +0,0 @@
-// // import { db } from "./firebase";
-// import { doc, getDoc } from "firebase/firestore";
-
-// export async function checkPermission(email: string) {
-//     const ref = doc(db, "allowedUsers", email);
-//     const snap = await getDoc(ref);
-//     return snap.exists(); // true -> allowed, false -> not allowed
-// }
